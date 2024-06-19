32b821d45dae59d8a1b35599c86ff6c15a8d70d3
Suyash Handa <87384376+Boolean-Autocrat@users.noreply.github.com>
2024-06-19 15:50:20 +0530

    [feat.] initializes project

---
+++ README.md
@@ -0,0 +1,12 @@
+# rm Util Node
+
+## Usage
+
+```bash
+node <filename> <file> [options]
+```
+
+### Options
+
+- `-r, --recursive`: Removes directories recursively.
+- `-f, --force`: Forces removal.
---
+++ rm-util.js
@@ -0,0 +1,34 @@
+const fs = require("fs");
+const path = require("path");
+const yargs = require("yargs");
+const { hideBin } = require("yargs/helpers");
+
+const argv = yargs(hideBin(process.argv))
+  .usage("Usage: $0 <file> [options]")
+  .command("$0 <file>", "Remove a file", (yargs) => {
+    yargs.positional("file", {
+      describe: "File to be removed",
+      type: "string",
+    });
+  })
+  .option("r", {
+    alias: "recursive",
+    type: "boolean",
+    description: "Removes directories recursively",
+  })
+  .option("f", {
+    alias: "force",
+    type: "boolean",
+    description: "Forces removal",
+  }).argv;
+
+const rmFileOrDirectory = (filePath, recursive = false, force = false) => {
+  try {
+    fs.rmSync(filePath, { recursive, force });
+    console.log("\x1b[32mSuccessfully removed file.\x1b[0m");
+  } catch (err) {
+    console.error(`\x1b[31mError: ${err.message}\x1b[0m`);
+  }
+};
+
+rmFileOrDirectory(path.resolve(argv.file), argv.recursive || false, argv.force || false);
